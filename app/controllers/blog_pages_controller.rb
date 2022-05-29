ebcdc3005828450c6e9935b54a4cc742213ce9ab
wishka <roucke@mail.ru>
2022-05-29 18:27:47 +0300

    fix home 4

--- app/controllers/blog_pages_controller.rb
+++ app/controllers/blog_pages_controller.rb
@@ -1,11 +1,8 @@
 class BlogPagesController < ApplicationController
 
   def home
-    if @carousel_rooms_first = Room.where(carousel: 'true').first.present? ||
-      @carousel_rooms_last = Room.where(carousel: 'true').last.present?
-    else
-      redirect_to root_path
-    end
+    @carousel_rooms_first = Room.where(carousel: 'true').first
+    @carousel_rooms_last = Room.where(carousel: 'true').last
     @rooms = Room.where(hot: 'true')
     @microposts = Micropost.all.limit(3)
     @telechannels = Telechannel.last(3)
