92473ef236b49aa859cc51c4a4b95da8a187b5b7
wishka <roucke@mail.ru>
2022-06-08 04:19:56 +0300

    update mail

--- config/initializers/cloudmailin.rb
+++ config/initializers/cloudmailin.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.add_delivery_method :cloudmailin, Mail::SMTP,
   address: ENV['CLOUDMAILIN_HOST'],
   port: 587,
-  domain: 'telegagid.herokuapp.com',
+  domain: '<telegagid>.herokuapp.com',
   user_name: ENV['CLOUDMAILIN_USERNAME'],
   password: ENV['CLOUDMAILIN_PASSWORD'],
   authentication: 'plain',
