876c372bf9b55a16a421b7117193ab0ac8603567
wishka <roucke@mail.ru>
2022-06-17 12:37:52 +0300

    change prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -67,16 +67,17 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'telegagid@heroku.trustifi.com' }
-  config.action_mailer.smtp_settings = {
-    :address => ENV'[TRUSTIFI_URL]',
-    :port => 587,
-    :enable_starttls_auto => true,
-    :user_name => ENV['TRUSTIFI_KEY'],
-    :password => ENV['TRUSTIFI_SECRET'],
-    :authentication => :plain
+  config.action_mailer.delivery_method = :mailgun
+  config.action_mailer.default_url_options = { host: 'telegagid@heroku.mailgun.com' }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'telegagid.herokuapp.com',
+    :authentication => :plain,
   }
+ActionMailer::Base.delivery_method = :smtp
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
--- config/initializers/cloudmailin.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.add_delivery_method :cloudmailin, Mail::SMTP,
-  address: ENV['CLOUDMAILIN_FORWARD_ADDRESS'],
-  port: 587,
-  domain: '<telegagid>.herokuapp.com',
-  user_name: ENV['CLOUDMAILIN_USERNAME'],
-  password: ENV['CLOUDMAILIN_SECRET'],
-  authentication: 'plain',
-  enable_starttls_auto: true
