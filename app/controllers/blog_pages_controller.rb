e95061803fe7f203fd50f6964ae37309b8ca1ad2
wishka <roucke@mail.ru>
2022-06-22 19:17:53 +0300

    Add carousel images

--- app/controllers/blog_pages_controller.rb
+++ app/controllers/blog_pages_controller.rb
@@ -2,6 +2,7 @@ class BlogPagesController < ApplicationController
 
   def home
     @carousel_rooms_first = Room.where(carousel: 'true').first
+    @carousel_rooms_second = Room.where(carousel: 'true').second
     @carousel_rooms_last = Room.where(carousel: 'true').last
     @rooms = Room.where(hot: 'true')
     @microposts = Micropost.all.limit(3)
